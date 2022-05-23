10f38d4ff80e60258268aaea34f0cee3f8d414eb
xcibe95x <xcibe95x@gmail.com>
2022-05-23 22:28:11 +0200

    Update JavaScript Molto Simpatico

--- assets/js/currentUser.js
+++ assets/js/currentUser.js
@@ -1,3 +1,4 @@
+// JS Facile Facile Plug and Play, Cosi possiamo toglierlo quando useremo ReactJS
 
 // Ho aggiunto questo file JS per fare la cosa dei profili che voleva fare davide
 // Per ora non modificatelo in nessun modo, lo ho già importato in Home.html
@@ -5,10 +6,12 @@
 // Ho chiesto il permesso ad Emanuele quindi non stiamo cheattando.
 
 // PHP $_GET Request Equivalent
-// Da utente di PHP ho rubato questo Gist, che mi crea quello che il comando $_GET['variabile']
+// Da utente di PHP ho rubato questo Gist, che mi crea quello che è il comando $_GET['variabile']
 // Su PHP permette di scrivere una variabile nell'url, e potremo passare il valore
-// Nella pagina html e sostituire l'immagine linkano diversi URL dalla index
+// Nella pagina html e sostituire l'immagine linkando diversi URL dalla index
 
+
+// Emulate PHP $_GET
 var $_GET = new Array();
 function GET() {
 	var url = location.search.replace("?", "").split("&");
@@ -19,6 +22,15 @@ function GET() {
 }
 GET();
 
+// Replace User Profile on Page Load
 window.onload = function() {
-document.getElementById("profile-picture").innerHTML = `<img src="./assets/img/profiles/ProfilePicture2.png" alt="Profileimg">`;
+	if ($_GET['user'] == null) {
+		document.getElementById("profile-picture").innerHTML = `<img src="./assets/img/profiles/ProfilePicture2.png" alt="Profileimg">`;
+	}
+}
+
+
+// Funzione Place Holder
+function AddGod(roba) {
+	return // LA FUNZIONA RITORNA XD
 }
